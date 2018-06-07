5fab25dfee87013e7d3c950cf138839d4469f666
rexbean <rexbean36@gmail.com>
2018-06-07 14:53:16 -0700

    add logging info

--- LoadBalancer/helpers/cmd_helper.py
+++ LoadBalancer/helpers/cmd_helper.py
@@ -10,16 +10,16 @@ def setNewDNS(request):
             s = socket.socket()
             s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
             s.connect((config.leader[0], config.leader[1]))
-            print('SENDING DNS SET TO RAFT')
+            config.log(1,"Received SET request: " + request)
             s.send(request+'\n')
             response = s.recv(1024)
 
             if not response:
-                print('there is reponse from cluster for set')
+                config.log(3,'there is reponse from cluster for set')
                 return False
             else:
-                print('response for set cmd:',response)
-                print('SEND MESSAGE BACK TO CLIENT')
+                config.log(3,response)
+                config.log(4,"Sending to Client")
                 config.client.send(response)
                 return True
         else:
@@ -34,24 +34,24 @@ def getIPAddr(request, algorithm):
         response = None
         req = json.loads(request)
         s = socket.socket()
+        config.log(1,"Received GET request: " + request)
         if req['leader'] == 'True':
-            print('sending to leader = ',config.leader)
+            config.log(2，'sending to leader', config.leader[1]）
             s.connect((config.leader[0], config.leader[1]))
         else:
             index = algorithm.roundRobin()
-            print('sending to the '+str(index) +'machine =', config.ipList[index])
+            config.log(2,'sending to the '+str(index), config.ipList[index])
             s.connect((config.ipList[index][0], config.ipList[index][1]))
-            print("Sending new Get Request to RAFT")
+            config.log(2,"Sending new Get Request to RAFT")
         s.send(request+'\n')
         response = s.recv(1024)
-        print('response ' ,response)
+        config.log(3,'response: ' + response)
 
         if not response:
-            print('there is no response from cluster for get')
+            config.log(3,'there is no response from cluster for get')
             return False
         else:
-            print(response)
-            print('SENDING THE RESPONSE BACK TO THE CLIENT')
+            config.log(4,'SENDING THE RESPONSE BACK TO THE CLIENT')
             config.client.send(response)
             return True
     except Exception as e:
@@ -66,17 +66,19 @@ def setNewLeader(request):
         electionTime = req['election_time']
         if electionTime > config.electionTime:
             config.leader = (leaderIP, leaderPort)
-            print(config.leader)
-            print('here is a new leader'+ str(leaderIP), leaderPort)
+            print('------------------------------------------------')
+            print('NEW LEADER DETECTED: '+ str(leaderIP), leaderPort)
+            print('------------------------------------------------')
             config.electionTime = electionTime
         return True
     except Exception as e:
-        print('set new Leader',e)
+        print("ERROR: " + e)
         return False
 
 
 def killNode(request, algorithm):
     try:
+        config.log(1,"Received kill request: " + request)
         req = json.loads(request)
         type = req['val']
         index = algorithm.roundRobin()
@@ -87,14 +89,13 @@ def killNode(request, algorithm):
                 index = algorithm.roundRobin()
 
         s = socket.socket()
-        print('sending killing message to the '+str(index) +'machine =', config.ipList[index])
+        config.log(2,'Sending KILL message to the '+str(index), config.ipList[index][1])
         s.connect((config.ipList[index][0], config.ipList[index][1]))
-        print('SEND KILLING MESSAGE TO RAFT')
         s.send(request+'\n')
         response = s.recv(1024)
         return True
     except Exception as e:
-        print('killing node ',e)
+        print(e)
         return False
 
 
--- LoadBalancer/helpers/config.py
+++ LoadBalancer/helpers/config.py
@@ -4,3 +4,14 @@ ipList = [("127.0.0.1",8080),("127.0.0.1",8090),("127.0.0.1",9000)]
 numServers = 3
 electionTime = 0
 round = 0
+
+def log(m_type,message,ip=None):
+    if m_type == 0:
+        header = "[1. FROM CLIENT]:"
+    else if m_type == 1:
+        header = "[2. TO RAFT ("+ip+")]:"
+    else if m_type == 2:
+        header = "[3. FROM RAFT]:"
+    else if m_type == 3：
+        header = "[4. TO CLIENT]:"
+    print header + " " + message
